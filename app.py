dcf1347519aa4890cfa59e0532af9cfccfd57920
tommy-mor <thmorriss@gmail.com>
2023-10-21 17:34:37 -0400

    progress

--- app.py
+++ app.py
@@ -4,8 +4,15 @@ with open("words.txt", 'r') as f:
 
 oxford_3000[4]
 
-import openai
-openai.api_key = "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"
+#import openai
+
+
+# read secret from secrets.json
+#import json
+#with open("secrets.json") as f:
+    #secrets = json.load(f)
+    
+#openai.api_key = secrets["api_key"]
 
 #word_embeddings_map = {}
 #
@@ -29,8 +36,8 @@ with open('embeddings.pickle', 'rb') as f:
     word_embeddings_map = pickle.load(f)
 
 import numpy as np
-from sklearn.decomposition import PCA
-import plotly.express as px
+#from sklearn.decomposition import PCA
+#import plotly.express as px
 
 words = np.array(list(word_embeddings_map.keys()))
 
@@ -40,63 +47,10 @@ words = np.array(list(word_embeddings_map.keys()))
 #embeddings = np.array(embeddings)
 embeddings = np.array(list(word_embeddings_map.values()))
 
-
-axis = [["small", "large"],
-        ["mean", "happy"],
-        ["sweet", "bitter"]]
-
 def find_embedding(word):
     print(word)
     index = np.where(words == word)[0][0]
     return embeddings[index]
-
-#x = find_embedding(axis[0][0]) - find_embedding(axis[0][1])
-#y = find_embedding(axis[1][0]) - find_embedding(axis[1][1])
-#z = find_embedding(axis[2][0]) - find_embedding(axis[2][1])
-#
-#xs = np.dot(embeddings, x)
-#ys = np.dot(embeddings, y)
-#zs = np.dot(embeddings, z)
-
-
-# find the vectors for each word
-# one dim is small - large, then each word is that dim cross product
-
-
-
-import plotly.graph_objects as go
-
-def visualize_3d_highlight(words_to_highlight):
-
-    # Default values for all words
-    colors = ['blue' if word not in words_to_highlight else 'red' for word in words]
-    opacities = [0.1 if word not in words_to_highlight else 1.0 for word in words]
-    sizes = [5 if word not in words_to_highlight else 10 for word in words]
-    texts = [word if word not in words_to_highlight else word for word in words]
-
-    # Create the scatter plot
-    scatter = go.Scatter3d(
-        x=xs,
-        y=ys,
-        z=zs,
-        mode='markers',
-        marker=dict(color=colors, size=sizes),
-        text=texts
-    )
-    layout = go.Layout(height=800)  # Set height as per your preference
-    fig = go.Figure(data=[scatter], layout=layout)
-    fig.show()
-
-# Call the function and pass the words you want to highlight
-words_to_highlight = ["life", "death", "awake", "asleep", "day", "night"] # replace with the words you want
-#visualize_3d_highlight(words_to_highlight)
-
-#import json
-#with open('reduced_embeddings.json', 'w') as f:
-    #json.dump(reduced_embeddings.tolist(), f)
-#
-#with open('words.json', 'w') as f:
-    #json.dump(words, f)
 
 from flask import Flask, jsonify, request
 app = Flask(__name__)
